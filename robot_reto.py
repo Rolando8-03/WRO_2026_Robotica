922e77edd33d6423cd205b12f4cc18c88406b75c
Jospeay <galeanolj@uamv.edu.ni>
2026-04-24 09:04:21 -0600

    Modificaciones del robot reto
    Hola rolando, ya lo subi

--- robot_reto.py
+++ robot_reto.py
@@ -9,12 +9,11 @@ print(robot.Hub.battery.voltage())
 robot.giro_izquierda(45, 620)
 robot.mover_recto(12, 900)
 robot.giro_izquierda(45, 620)
-robot.mover_recto(10, 900)
 robot.esperar(10)
 #En este punto ya está acomodado para buscar la línea
 
 # ===================== SECCIÓN 2 (CEMENTO) =====================
-robot.seguir_linea(distancia_cm=60, velocidad=1000, ref=30)
+robot.seguir_linea(robot.seguidor, 100, 65)
 robot.girar(-90, 450)
 robot.esperar(10)
 robot.avanzar_con_torque(-19, 160, 700, 140)
@@ -24,57 +23,65 @@ robot.girar(80)
 robot.retroceder_recto(40, 650)
 #Aqui ya dejó la paleta de albañilería
 
+
 # ===================== TRANSICIÓN =====================
 robot.mover_recto(30.5, 900)
-robot.giro_derecha(-41, 620)
-robot.giro_izquierda(53, 620)
-robot.seguir_linea(distancia_cm=37, velocidad=1000, ref=30)
+robot.esperar(100)
+robot.girar(-14, 500)
+robot.mover_recto(8,800)
+robot.girar(20, 500)
+robot.esperar(120)
+robot.seguir_linea(robot.seguidor, 100, 38)
 #Aqui ya llegó al lugar de estacionamiento
 
 # ===================== ACCIÓN =====================
 robot.esperar(10)
 robot.girar(91, 500)
-robot.mover_torque(-170, 1000)
+robot.mover_torque(-70, 1000)
 robot.retroceder_recto(10, 650) #empujar el cemento
 #Aqui ya dejó el cemento en el lugar de estacionamiento
 
 # ===================== SECCIÓN FINAL =====================
-robot.mover_recto(13.2, 900)
-robot.girar(-89.5, 450)
-robot.mover_recto(6,900)
-robot.seguir_linea(distancia_cm=9, velocidad=1000, ref=30)
-robot.girar(-179, 500)
-robot.avanzar_con_torque(-22, 165, 750, 300)
+robot.mover_recto(10, 900)
+robot.giro_derecha(-85, 450)
+robot.esperar(100)
+
+robot.mover_recto(5, 800)
+robot.seguir_linea(robot.seguidor, 100, 10)
+robot.girar(-175, 500)
+robot.avanzar_con_torque(-24, 165, 750, 300)
 #Aqui agarra los cementos blancos
 
 # ===================== SALIDA =====================
 robot.mover_recto(10, 850)
-robot.girar(65, 450)
-robot.mover_recto(51)
-robot.girar(-63, 500)
+robot.girar(77, 450)
+robot.mover_recto(47) #MOVER RECTO HASTA SEGUIR LA LINEA A LA MATRIZ
+robot.girar(-64, 500)
 robot.esperar(100)
-robot.seguir_linea(distancia_cm=20, velocidad=1000, ref=30)
-#Aqui se posicionó enfrente de la matriz
 
+robot.seguir_linea(robot.seguidor, 60, 26) #SEGUIR LINEA PARA IR A LA MATRIZ
+#Aqui se posicionó enfrente de la matriz
 robot.esperar(100)
-robot.girar(-176, 500)
-robot.retroceder_recto(8.5, 700)
-robot.giro_derecha(43, 450)
-robot.avanzar_con_torque(-22, -150, 700, 180)  # dejar los cementos blancos
+robot.girar(-182, 500)
+robot.retroceder_recto(10, 900)
+robot.girar(38, 500)
+robot.avanzar_con_torque(-26, -150, 700, 180)  # DEJAR LOS CEMENTOS BLANCOS
 #Aqui ya dejó los cementos blancos
 
-robot.mover_recto(21, 900)
-robot.giro_derecha(-51, 450)
+robot.mover_recto(19, 900)
+robot.girar(-45, 450)
 robot.esperar(100)
 #Aqui ya se posiciona para detectar la matriz
 
-robot.avanzar_con_torque(-14, 190, 700, 100)
-robot.esperar (10)
-robot.iden_matriz()
-robot.mover_torque(-170, 1000)
+robot.seguir_linea(robot.seguidor, 60, 7)
+robot.retroceder_recto(20, 700)
+robot.mover_torque(70, 100)
+robot.esperar(1000)
+robot.retroceder_recto(2, 700)
+robot.mover_torque(-70,100)
+robot.mover_recto(10, 800)
 robot.esperar(10)
-robot.seguir_linea(distancia_cm=35, velocidad=1000, ref=40)
-
+robot.seguir_linea(robot.seguidor, 100, 35)
 
 '''
 #-----------------------------
