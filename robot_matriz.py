ef31de2f516740821477ddbd9117e405d99735e1
Rolando Mayorga <remena@uamv.edu.ni>
2026-05-08 19:01:46 -0600

    Update robot movement logic in robot_matriz.py
    Adjusted movement parameters and added new commands for the second matrix.

--- robot_matriz.py
+++ robot_matriz.py
@@ -11,7 +11,7 @@ robot = Base()
 print(robot.Hub.battery.voltage())
 
 #----------------- Combinación #4 -----------------------
-
+'''
 #----- Primera Fase: Dejar los primeros 4 bloques -------
 robot.seguir_linea(robot.seguidor,100,52.5)
 robot.mover_garra(1000, 110)
@@ -40,12 +40,12 @@ robot.girar(92,500)
 robot.seguir_linea(robot.seguidor,100,19)
 robot.girar(90,500)
 robot.mover_recto(12,900)
-
+'''
 #CODIGO PARA LA PRIMERA MATRIZ
 robot.seguir_linea(
     robot.seguidor,
     velocidad_max=100,
-    distancia_cm=30.5,
+    distancia_cm=31.5,
     kp=0.48,
     kd=1.70,
     k_freno=0.52,
@@ -63,11 +63,32 @@ robot.girar(-89, 500)
 
 robot.mover_garra_delantera(300, -286) #velocidad, grados
 robot.mover_recto(24.5, 850)
-robot.mover_garra_delantera(300, 286) #velocidad, grados
+robot.mover_garra_delantera(300, 279) #velocidad, grados
 robot.retroceder_recto(14.5, 600)
 robot.girar(89, 500)
-robot.mover_recto(10, 800)
-robot.girar(-89, 500)
-robot.mover_recto(10, 900)
+robot.mover_recto(8.75, 800)
+robot.girar(-88, 500)
+robot.mover_garra(150, 105)
+robot.mover_garra_delantera(300, -60) #velocidad, grados
+robot.mover_recto(16.5, 900)
+robot.mover_garra_delantera(300, 63) #velocidad, grados
+robot.mover_garra(150, 25) 
+robot.retroceder_recto(30,600)
+robot.girar(90,500)
+robot.mover_garra(150,-130)
+robot.mover_garra_delantera(300, -286) #velocidad, grados
+robot.retroceder_recto(20,600)
+robot.mover_garra_delantera(300, 286) #velocidad, grados
+robot.mover_recto(22,900)
+robot.mover_garra(150,115)
+robot.esperar(10)
+robot.mover_garra(150,-115)
+robot.retroceder_recto(13,600)
+robot.mover_garra_delantera(300, -286) #velocidad, grados
 
 
+#Segunda matriz
+robot.girar(-91,500)
+robot.mover_garra(150,130)
+robot.mover_recto(40,900)
+robot.mover_garra_delantera(300, 286) #velocidad, grados
