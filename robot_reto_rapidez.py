ebd1fe7ffa5be78eac567f3e2240ce34c7ffef8d
elyGarcia <elgarcia@uamv.edu.ni>
2026-05-12 09:46:45 -0600

    Update robot_reto_rapidez.py
    ultima versión del reto (hasta los cementos blancos)

--- robot_reto_rapidez.py
+++ robot_reto_rapidez.py
@@ -1,4 +1,4 @@
-from robot_control import Base
+from robot_control_prueba import Base
 from pybricks.parameters import Color, Direction, Port, Stop
 from matriz import ejecutar_matriz
 
@@ -7,14 +7,17 @@ robot = Base()
 print(robot.Hub.battery.voltage())
 matriz_detectada = None
 
+
 # ===================== SECCIÓN 1 (TOMAR CEMENTO Y UBICAR PALA) =====================
-#SECCION 1.1 -> SALIDA Y AVANZAR HASTA EL BALDE DE CEMENTO
+
+# SECCIÓN 1.1 -> SALIDA Y AVANZAR HASTA EL BALDE DE CEMENTO
 robot.giro_arco_dc(
     radio_cm=9.6,
     angulo_deg=140,
     potencia=100,
     lado="derecha"
 )
+
 robot.seguir_linea_extremo(
     sensor_color=robot.seguidor,
     velocidad_max=100,
@@ -23,222 +26,152 @@ robot.seguir_linea_extremo(
     tiempo_acomodo_ms=80,
     kp=1.15,
     kd=2.6,
-    k_freno=0.0
+    k_freno=0.0,
+    perfil_salida="encadenado"
 )
-#SECCION 1.2 -> AGARRAR EL BALDE DE CEMENTO
-robot.girar_rapido_preciso(-90, velocidad=950, velocidad_min=160, anticipacion=8)
-'''
-robot.retroceder_recto_rapido(9, 950)
-'''
-robot.avanzar_con_torque(-17.5, -168, 1200, 200) 
-robot.mover_recto_rapido(0.5, 1000)
 
-#SECCION 1.3 -> DEJAR LA PALA DE ALBAÑILERIA
-robot.girar_rapido_preciso(80, velocidad=950, velocidad_min=160, anticipacion=8)
-robot.retroceder_recto_rapido(distancia_cm=42, velocidad=950)
-robot.mover_recto_rapido(distancia_cm=31, velocidad=1200)
+
+# SECCIÓN 1.2 -> AGARRAR EL BALDE DE CEMENTO
+robot.girar(
+    -90,
+    velocidad=1000,
+    velocidad_min=160,
+    anticipacion=8,
+    perfil="encadenado"
+)
+
+robot.avanzar_con_torque(
+    distancia_cm=-17.5,
+    grados_torque=-169,
+    velocidad_robot=1200,
+    velocidad_torque=350,
+    torque_despues_cm=1,
+    esperar_torque=False,
+    perfil_entrada="encadenado",
+    perfil_salida="encadenado"
+)
+
+
+robot.mover_recto(
+    distancia_cm=1,
+    velocidad=1000,
+    perfil="encadenado"
+)
+
+
+# SECCIÓN 1.3 -> DEJAR LA PALA DE ALBAÑILERÍA
+robot.girar(
+    69.5,
+    velocidad=1000,
+    velocidad_min=160,
+    anticipacion=8,
+    perfil="encadenado"
+)
+
+robot.retroceder(
+    distancia_cm=42,
+    velocidad=870,
+    perfil="seguro",
+    invertir_correccion=False,
+    pausa_gyro=25
+)
+
+
+robot.mover_recto(
+    distancia_cm=36,
+    velocidad=1200,
+    perfil="encadenado"
+)
+
+'''
 robot.giro_arco_dc(
-    radio_cm=9,
+    radio_cm=13,
     angulo_deg=45,
     potencia=80,
     lado="izquierda"
 )
-
+'''
 robot.giro_arco_dc(
-    radio_cm=10,
-    angulo_deg=50,
+    radio_cm=13,
+    angulo_deg=19,
     potencia=80,
     lado="derecha"
 )
-#===========================SECCION#2 (DEJAR EL CEMENTO)==============================
-#SECCION 2.1 -> AVANZAR HASTA DEJAR EL CEMENTO
-robot.esperar(120)
+
+
+# ===================== SECCIÓN 2 (DEJAR EL CEMENTO) =====================
+
+# SECCIÓN 2.1 -> AVANZAR HASTA DEJAR EL CEMENTO
 robot.seguir_linea_extremo(
     sensor_color=robot.seguidor,
     velocidad_max=100,
-    distancia_cm=31,
+    distancia_cm=39,
     lado="derecha",
     tiempo_acomodo_ms=80,
     kp=1.15,
     kd=2.6,
-    k_freno=0.0
+    k_freno=0.0,
+    perfil_salida="encadenado"
 )
 
-# SECCION 2.2 -> PONER EL CEMENTO EN EL ESTACIONAMIENTO
-robot.girar_rapido_preciso(90, velocidad=950, velocidad_min=160, anticipacion=8)
-robot.avanzar_con_torque(-10, 166, 1200, 200) 
 
-# ===================== SECCIÓN #3 (IR POR LOS CEMENTOS BLANCOS) =====================
-# SECCION 3.1 -> POSICIONARSE ENFRENTE DE LA LÍNEA DEL SEGUIDOR
-robot.mover_recto(7, 900)
-robot.giro_derecha(-90, 550)
-robot.esperar(100)
+# SECCIÓN 2.2 -> PONER EL CEMENTO EN EL ESTACIONAMIENTO
+robot.girar(
+    90,
+    velocidad=950,
+    velocidad_min=160,
+    anticipacion=8,
+    perfil="encadenado"
+)
 
-#SECCION 3.2 -> AGARRAR LOS CEMENTOS BLANCOS
-robot.seguir_linea(
-    robot.seguidor,
+robot.avanzar_con_torque(
+    distancia_cm=-9,
+    grados_torque=166,
+    velocidad_robot=1200,
+    velocidad_torque=200,
+    esperar_torque=False,
+    perfil_entrada="encadenado",
+    perfil_salida="encadenado"
+)
+
+
+# ===================== SECCIÓN 3 (IR POR LOS CEMENTOS BLANCOS) =====================
+
+# SECCIÓN 3.1 -> POSICIONARSE ENFRENTE DE LA LÍNEA DEL SEGUIDOR
+robot.giro_derecha(
+    -90,
+    velocidad=1200,
+    velocidad_min=160,
+    anticipacion=10,
+    perfil="encadenado"
+)
+
+robot.seguir_linea_extremo(
+    sensor_color=robot.seguidor,
     velocidad_max=100,
     distancia_cm=9,
-    kp=0.48,
-    kd=1.70,
-    k_freno=0.52,
-    tiempo_acomodo_ms=400
-)
-robot.girar(-180, 500)
-robot.avanzar_con_torque(-20, -176, 1000, 400)
-
-# ===================== SECCION #4 (DEJAR LOS CEMENTOS) =====================
-#SECCION 4.1 -> POSICIONARSE EN LA LÍNEA FRENTE A LA MATRIZ
-robot.mover_recto(10, 850)
-robot.girar(70, 450)
-robot.mover_recto(50.5) 
-robot.girar(-70, 500) 
-robot.esperar(100)
-
-robot.seguir_linea(
-    robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=38,
-    lado="izquierda",
-    kp=0.48,
-    kd=1.70,
-    k_freno=0.52,
-    tiempo_acomodo_ms=400
+    lado="derecha",
+    tiempo_acomodo_ms=80,
+    kp=1.15,
+    kd=2.6,
+    k_freno=0.0,
+    perfil_salida="encadenado"
 )
 
-#SECCION 4.2 -> DEJAR LOS CEMENTOS BLANCOS CON GIRO
-robot.esperar(100)
-robot.mover_recto(23.5, 700) #entrar en la matriz
-robot.esperar(500)
-matriz_detectada = robot.escanear_matriz()
-print("Matriz guardada:", matriz_detectada)
-
-robot.retroceder_recto(38, 800)
-robot.girar(180, 500) 
-robot.retroceder_recto(14, 900)
-robot.girar(49, 500)
-robot.avanzar_con_torque(-20, 145, 900, 200) #DEJAR LOS CEMENTOS BLANCOS
-robot.esperar(100)
-
-#==============================SECCION #5 (Avance para buscar la linea )==========================================
-robot.mover_recto(17,900)
-robot.girar(-44, 550)
-
-#==================================SECCION #6 (IR POR LOS CEMENTOS VERDES) ==============================
-
-#SECCION 6.1 -> AVANZA PARA IR A BUSCAR LOS CEMENTOS VERDES Y HACE GIRO PARA AGARRARLOS 
-robot.seguir_linea(
-    robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=37,
-    kp=0.48,
-    kd=1.70,
-    k_freno=0.52,
-    tiempo_acomodo_ms=400
-)
-robot.girar(180, 500) 
-robot.avanzar_con_torque(-29, -150, 900, 400)
-
-#SECCION 6.2 -> AVANZA PARA IR A DEJAR LOS CEMENTOS VERDES Y HACE GIRO PARA DEJARLO
-robot.mover_recto(5, 900)
-robot.seguir_linea(
-    robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=38,
-    lado="izquierda",
-    kp=0.48,
-    kd=1.70,
-    k_freno=0.52,
-    tiempo_acomodo_ms=400
+robot.girar(
+    -160,
+    velocidad=1200,
+    velocidad_min=160,
+    anticipacion=0,
+    perfil="encadenado"
 )
 
-robot.girar(-180, 500)
-robot.avanzar_con_torque(-16, 175, 900, 400) #AQUI DEJA LOS VERDES
-
-#==================================SECCION #7 (IR POR LOS CEMENTOS AMARILLOS) ==============================
-#SECCION 7.1 -> SEGUIR LA LINEA PARA IR A TOMA LOS CEMENTOS AMARILLOS (EN ESPERA DE MEJOR PRECISION)
-robot.mover_recto(9, 900)
-robot.girar(-43, 500)
-robot.mover_recto(34, 900)
-robot.girar(44, 500)
-robot.seguir_linea(
-    robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=9,
-    kp=0.48,
-    kd=1.70,
-    k_freno=0.52,
-    tiempo_acomodo_ms=400
+robot.avanzar_con_torque(
+    distancia_cm=-20,
+    grados_torque=-168,
+    velocidad_robot=1200,
+    velocidad_torque=250,
+    esperar_torque=False,
+    perfil_entrada="encadenado",
+    perfil_salida="encadenado"
 )
-robot.girar(-180, 400)
-robot.avanzar_con_torque(-18, -170, 1000, 400) #TOMAR LOS CEMENTOS AMARILLOS
-
-#SECCION 7.2 -> AVANCES MECANICOS PARA IR A ACOMODARSE A UN LINEA PARA IR A DEJAR LOS CEMENTOS AMARILLOS
-robot.mover_recto(10, 900)
-robot.girar(-75, 500)
-robot.mover_recto(69, 900) #IR PARA SEGUIR LA LINEA AL LUGAR DE LOS AMARILLOS
-robot.girar(76, 500)
-robot.seguir_linea(
-    robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=56,
-    lado="izquierda",
-    kp=0.48,
-    kd=1.70,
-    k_freno=0.52,
-    tiempo_acomodo_ms=400
-)
-
-#SECCION 7.3 -> POSICIONAMIENTO PARA DEJAR EN SU LUGAR LOS CEMENTOS AMARILLOS
-robot.girar(-90, 500)
-robot.avanzar_con_torque(-17, 175, 900, 160)
-robot.mover_recto(14, 900) 
-
-#==================================SECCION #8 (IR POR LOS CEMENTOS AZULES) ==============================
-#SECCION 8.1 -> GIRO PARA ACOMODARSE EN LA LINEA PARA BUSCAR LOS CEMENTOS AZULES + GIRO PARA IR DIRECTO A LOS CEMENTOS Y RECOGERLOS
-robot.girar(-90, 500)
-robot.seguir_linea(
-    robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=55,
-    kp=0.48,
-    kd=1.70,
-    k_freno=0.52,
-    tiempo_acomodo_ms=400
-)
-robot.girar(140, 500)
-robot.retroceder_recto(27, 900)
-robot.girar(40,500)
-robot.avanzar_con_torque(-20, -175, 900, 400) #TOMAR LOS CEMENTOS AZULES
-
-#SECCION 8.2 -> AVANCES MECANICOS PARA IR A AGARRAR LA PALA Y PODER LLEVARLA AL INICIO JUNTO LOS AZULES 
-robot.mover_recto(10, 900)
-robot.girar(-29, 500)
-robot.mover_recto(44.5, 900)
-robot.mover_garra(400, -61) #abrir la garra
-robot.mover_garra_delantera(1000, 265) #velocidad, grados
-robot.mover_garra(400, 61)
-
-#SECCION 8.3 -> ACOMODACION PARA SEGUIR LINEA Y IR A DEJAR LOS CEMENTOS JUNTO CON LA PALA 
-robot.retroceder_recto(8.5, 600)
-robot.giro_derecha(25, 500)
-robot.seguir_linea(
-    robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=140,
-    lado="izquierda",
-    kp=0.48,
-    kd=1.70,
-    k_freno=0.52,
-    tiempo_acomodo_ms=400
-)
-
-#SECCION 8.4 -> POSICIONAMIENTO DE LA PALA CON LIGERO GIRO Y DESPUES APERTURA DE LA CELDA PARA DEJAR LOS CEMENTOS AZULES
-robot.mover_garra(400, -120)
-robot.retroceder_recto(12.5, 600)
-robot.girar(90, 500)
-robot.mover_torque(176, 500)
-robot.mover_garra_delantera(500, -265)
-robot.mover_garra(400, 130)
-
-ejecutar_matriz(robot, matriz_detectada)
