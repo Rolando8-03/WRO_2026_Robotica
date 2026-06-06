cdb00ff8ab92302e43f9ef1c041d8c5041a3c146
Jospeay <galeanolj@uamv.edu.ni>
2026-06-06 11:44:50 -0600

    Refactor matrix execution logic in matriz2.py

--- matriz2.py
+++ matriz2.py
@@ -1,143 +1,67 @@
 def ejecutar_matriz_2(robot):
     print("Ejecutando recorrido de matriz 2")
-"Inicio del recorrido para ir a buscar el primer color para llevar del mosaico seleccionado
-    robot.seguir_linea_extremo(
-    sensor_color=robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=34,
-    lado="derecha",
 
-    tiempo_acomodo_ms=140,
-    tiempo_aceleracion_ms=140,
+    robot.seguir_linea(36)
+    robot.esperar(300)
+    robot.girar(-80)
+    robot.mover_garra(300)
+    robot.seguir_linea(7)
+    robot.esperar(100)
+    robot.avanzar(12.5)
 
-    kp=1.25,
-    kd=2.7,
-    k_freno=0.16,
-    correccion_max=100,
+    robot.mover_garra_delantera(293, velocidad=800)
+    robot.retroceder(22)
+    robot.esperar(300)
+    robot.girar(-80)
+    robot.seguir_linea(12)
 
-    objetivo_reflexion=27,
-    captura_inicial=True,
-    tiempo_captura_ms=280,
-    potencia_captura=60,
-    kp_captura=2.5,
+    robot.esperar(300)
+    robot.girar(79)
+    robot.mover_garra_delantera(-30, velocidad=600)
+    robot.mover_garra(500)
 
-    perfil_salida="encadenado"
-)
+    robot.avanzar(14)
+    robot.mover_garra_delantera(30, velocidad=600)
+    robot.mover_garra(500)
 
-"Giro para ponerse frente a los colores"
-robot.esperar(300)
-robot.girar(-84, velocidad=900, velocidad_min=160, anticipacion=8, perfil="encadenado")
+    robot.retroceder(3)
+    robot.retroceder(11)
+    robot.esperar(500)
+    robot.girar(75)
+    robot.seguir_linea(18)
+    robot.esperar(500)
+    robot.girar(80)
+    robot.seguir_linea(13)
 
-"Seguidor para acomodarse recto y utilizar un avance mecanico para alcanzar los 4 bloques y utilizar el torque para agarrarlos"
-robot.mover_garra(300, 90, esperar= False)
-robot.seguir_linea_extremo(
-    sensor_color=robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=7,
-    lado="derecha",
+    robot.mover_garra(500, 80)
+    robot.mover_garra_delantera(600, -90)
+    robot.esperar(300)
+    robot.retroceder(12)
+    robot.mover_garra_delantera(600, 80)
+    robot.avanzar(14)
+    robot.mover_garra(500)
 
-    tiempo_acomodo_ms=140,
-    tiempo_aceleracion_ms=140,
+    robot.mover_garra_delantera(600, -50)
+    robot.seguir_linea(10)
+    robot.esperar(100)
 
-    kp=1.25,
-    kd=2.7,
-    k_freno=0.16,
-    correccion_max=100,
+    robot.mover_garra_delantera(500, -250)
+    robot.esperar(300)
+    robot.avanzar(13)
+    robot.mover_garra_delantera(400, 235)
 
-    objetivo_reflexion=27,
-    captura_inicial=True,
-    tiempo_captura_ms=280,
-    potencia_captura=60,
-    kp_captura=2.5,
+    robot.mover_garra(100)
 
-    perfil_salida="encadenado"
-)
-robot.esperar(100)
-robot.mover_recto( distancia_cm=10, velocidad=1000, perfil="encadenado")
-robot.mover_garra_delantera(600, 293)
+'''
+#Para pruebas solo con la matriz descomentar esta parte
+from robot import Robot
 
-"Retroceso mas giros para ir a buscar la otra parte de la mitad del mosaico, junto con seguidor de linea para poder acomodarse recto"
-robot.esperar(500)
-robot.retroceder(distancia_cm=23, velocidad=700, perfil="seguro", invertir_correccion=False, pausa_gyro=25)
-robot.esperar(500)
-robot.girar(-90, velocidad=900, velocidad_min=160, anticipacion=8, perfil="encadenado")
-robot.seguir_linea_extremo(
-    sensor_color=robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=7,
-    lado="izquierda",
+def main():
+    robot = Robot()
+    robot.mostrar_bateria()
 
-    tiempo_acomodo_ms=140,
-    tiempo_aceleracion_ms=140,
+    ejecutar_matriz_1(robot)
 
-    kp=1.25,
-    kd=2.7,
-    k_freno=0.16,
-    correccion_max=100,
-
-    objetivo_reflexion=27,
-    captura_inicial=True,
-    tiempo_captura_ms=280,
-    potencia_captura=60,
-    kp_captura=2.5,
-
-    perfil_salida="encadenado"
-)
-
-"Giros para posicionarse frente a los colores, que en esta ocasion son dos amarillos y agarrarlos"
-robot.esperar(500)
-robot.girar(90, velocidad=800, velocidad_min=160, anticipacion=8, perfil="encadenado")
-robot.esperar(500)
-robot.mover_garra(300,-45, esperar= False, apretar= False)
-robot.esperar(500)
-robot.mover_recto( distancia_cm=14, velocidad=900, perfil="encadenado")
-robot.mover_garra(300,-35, esperar= False, apretar= False)
-robot.retroceder(distancia_cm=3, velocidad=600, perfil="seguro", invertir_correccion=False, pausa_gyro=25)
-
-
-"Retroceso con diferentes movimientos para tener espacio para poder acomodar los 6 bloques y poder llevarlos hacia el mosaico"
-robot.retroceder(distancia_cm=14, velocidad=700, perfil="seguro", invertir_correccion=False, pausa_gyro=25)
-robot.mover_garra(300, 80, esperar= False)
-robot.mover_garra_delantera(600, -70)
-robot.retroceder(distancia_cm=14, velocidad=700, perfil="seguro", invertir_correccion=False, pausa_gyro=25)
-robot.mover_garra_delantera(600, 60)
-robot.mover_recto( distancia_cm=18, velocidad=1000, perfil="encadenado")
-robot.mover_garra(500,-50, esperar= False, potencia_apriete=70)
-robot.retroceder(distancia_cm=6, velocidad=700, perfil="seguro", invertir_correccion=False, pausa_gyro=25)
-
-"Giros y avance recto para ir a posicionarse frente al mosaico"
-robot.esperar(500)
-robot.girar(90, velocidad=700, velocidad_min=160, anticipacion=8, perfil="encadenado")
-robot.mover_recto( distancia_cm=16, velocidad=1000, perfil="encadenado")
-robot.esperar(500)
-robot.girar(85, velocidad=700, velocidad_min=160, anticipacion=8, perfil="encadenado")
-
-"Seguidor para quedar recto frente al mosaico"
-robot.seguir_linea_extremo(
-    sensor_color=robot.seguidor,
-    velocidad_max=100,
-    distancia_cm=12,
-    lado="derecha",
-
-    tiempo_acomodo_ms=140,
-    tiempo_aceleracion_ms=140,
-
-    kp=1.25,
-    kd=2.7,
-    k_freno=0.16,
-    correccion_max=100,
-
-    objetivo_reflexion=27,
-    captura_inicial=True,
-    tiempo_captura_ms=280,
-    potencia_captura=60,
-    kp_captura=2.5,
-
-    perfil_salida="encadenado"
-)
-"Avance mecanico con levantamiento de garra con los 6 bloques y dejarlos en el mosaico"
-robot.esperar(100)
-robot.mover_garra_delantera(200, -60)
-robot.mover_recto( distancia_cm=28, velocidad=800, perfil="encadenado")
-robot.retroceder(distancia_cm=8, velocidad=700, perfil="seguro", invertir_correccion=False, pausa_gyro=25)
-robot.mover_garra(500,55, esperar= False)
+if __name__ == "__main__":
+    main()
+'''
