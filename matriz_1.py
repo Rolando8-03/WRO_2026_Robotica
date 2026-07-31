a6e7d4ff3decb16ce5d20b5bcca04d9c94a6f196
Rolando Mayorga <remena@uamv.edu.ni>
2026-07-31 16:27:22 -0600

    Refactor ejecutar_matriz_1 function and adjust parameters

--- matriz_1.py
+++ matriz_1.py
@@ -1,26 +1,27 @@
-from control_drivebase import Base
+#from control_drivebase import Base
 from pybricks.parameters import Color
 from pybricks.tools import wait
 
-def ejecutar_matriz_1(robot):
+def ejecutar_matriz_1(self, robot):
 
     print("Voltaje:", robot.Hub.battery.voltage(), "mV")
     print("Ejecutando recorrido de matriz 1")
-    
+    robot.motor_garra_delantera.reset_angle(0)
+
     robot.avanzar_cruzando_lineas(cruces_objetivo=3,velocidad=900,retraso_freno_ms=95.5)
-    robot.mover_garra_principal(850, 130)
+    robot.mover_garra_principal(850,100)
     robot.girar(-90, potencia_max=100)
     robot.avanzar_recto(5, 800)
-    robot.mover_garra_delantera(850, 286)
+    robot.mover_garra_delantera(280)
     #Aqui ya agarro los primeros dos bloques verdes
-
+    
     robot.avanzar_recto(-8, 800)
     robot.girar(-90, potencia_max=100)
     robot.avanzar_recto(13)
     robot.girar(92, potencia_max=100)
-    robot.mover_garra_delantera(850, -286)
+    robot.mover_garra_delantera(0)
     robot.avanzar_recto(8, 800)
-    robot.mover_garra_delantera(850, 276)
+    robot.mover_garra_delantera(280)
     wait(300)
     #Aqui ya agarro los dos bloques azules
 
@@ -28,7 +29,7 @@ def ejecutar_matriz_1(robot):
     robot.girar(88, potencia_max=100)
     robot.avanzar_recto(9.5)
     robot.girar(-90, potencia_max=80)
-    robot.mover_garra_principal(850, -105,apretar=False)
+    robot.mover_garra_principal(850,-35,apretar=False)
     robot.avanzar_recto(11)
     robot.mover_garra_principal(850, -30,apretar=True)
     #Aqui ya agarro los ultimos dos bloques de en medio de la matriz
@@ -42,6 +43,7 @@ def ejecutar_matriz_1(robot):
     
     robot.dejar_bloques_matriz(robot)
     
+    robot.avanzar_recto(-12)
     robot.girar(-170)
     robot.avanzar_recto(5)
     robot.seguir_linea(distancia_cm=17)
@@ -50,16 +52,16 @@ def ejecutar_matriz_1(robot):
     robot.avanzar_recto(6.5)
     robot.girar(90)
     robot.avanzar_recto(16)
-    robot.mover_garra_delantera(850, 176)
+    robot.mover_garra_delantera(280)
     #Aqui ya agarró los últimos 4 bloques azules
 
     robot.avanzar_recto(-18)
     robot.girar(90)
     robot.avanzar_recto(16)
-    robot.mover_garra_principal(850, -60,apretar=False)
+    robot.mover_garra_principal(850, 35,apretar=False)
     robot.girar(-90)
     robot.avanzar_recto(10)
-    robot.mover_garra_principal(850, -30)
+    robot.mover_garra_principal(850, -36,potencia_apriete=40)
     #Aqui ya agarró los últimos dos bloques verdes de la matriz
     
     robot.avanzar_recto(-11)
@@ -69,8 +71,10 @@ def ejecutar_matriz_1(robot):
     robot.avanzar_recto(3)
     #Aqui se coloca en la línea del seguidor para la matriz
     
-    robot.dejar_bloques_matriz(robot, distancia_matriz=5,dis_retr=-16)
-
+    robot.dejar_bloques_matriz2(robot)
+    robot.avanzar_recto(-20)
+    robot.girar(-170)
+    '''
     robot.giro_izquierda(-90.5)
     robot.avanzar_recto(-28)
     robot.mover_torque(grados_torque=-170, velocidad_torque=500)
@@ -78,7 +82,9 @@ def ejecutar_matriz_1(robot):
     robot.girar(-90,potencia_max=100)
     robot.avanzar_recto(-22)
     #Aqui ya termina el reto con los azules
-    
+    '''
+'''
 if __name__ == "__main__":
     robot = Base()
     ejecutar_matriz_1(robot)
+'''
