dfe956cbd5adb7ce64cc945229fbd7e739c19473
elyGarcia <elgarcia@uamv.edu.ni>
2026-08-22 13:58:49 -0600

    Update reto_1.py

--- reto_1.py
+++ reto_1.py
@@ -10,6 +10,8 @@ from pybricks.parameters import Color
 from pybricks.tools import wait
 import gc 
 from matriz_2 import ejecutar_matriz_2
+from matriz_3 import ejecutar_matriz_3
+
 robot = Base()
 print("Voltaje:", robot.Hub.battery.voltage())
 
@@ -28,12 +30,12 @@ gc.collect()
 
 robot.seguir_linea(
     sensor_color=robot.seguidor,
-    distancia_cm=82,           
+    distancia_cm=79,           
     velocidad_max=100,         
     lado="derecha",            
     
     tiempo_acomodo_ms=50,      
-    tiempo_aceleracion_ms=50,  
+    tiempo_aceleracion_ms=80,  
     
     #CEREBRO PREDICTIVO (PID):
     kp=1.15,                   
@@ -43,7 +45,7 @@ robot.seguir_linea(
     correccion_max=100,
     objetivo_reflexion=27,     
     
-    captura_inicial=True,   
+    captura_inicial=True,
     tiempo_captura_ms=280,
     potencia_captura=60,
     kp_captura=2.5,
@@ -62,6 +64,7 @@ robot.avanzar_recto(distancia_cm=-16, velocidad_max=1000, perfil="encadenado")
 # Pequeño avance despues de tomar el cemento y giro en direccion a la llana
 robot.avanzar_recto(distancia_cm=6, velocidad_max=1000, perfil="encadenado")
 robot.girar(angulo_deg=77, potencia_max=100, perfil="encadenado") 
+
 
 # Retroceso para dejar la llana en su lugar y mov.recto para regresar a la linea
 robot.avanzar_recto(distancia_cm=-32, velocidad_max=1000, perfil="seguro")
@@ -88,7 +91,7 @@ robot.giro_de_arco(
 # Seguir linea hasta lugar de cemento. Giro y movimiento torque para dejarlo en su lugar
 robot.seguir_linea(
     sensor_color=robot.seguidor,
-    distancia_cm=42,           
+    distancia_cm=43,           
     velocidad_max=100,         
     lado="derecha",            
     
@@ -211,7 +214,7 @@ robot.avanzar_recto(distancia_cm=-37.5, velocidad_max=900, perfil="seguro")
 
 robot.girar(angulo_deg=-181, potencia_max=100, perfil="encadenado")
 robot.avanzar_recto(distancia_cm=-23.5, velocidad_max=670, perfil="seguro") 
-robot.avanzar_recto(distancia_cm=8, velocidad_max=900, perfil="seguro") 
+robot.avanzar_recto(distancia_cm=7.5, velocidad_max=900, perfil="seguro") 
 
 # Giro para entrar en los cementos blancos y dejarlos =====================
 robot.girar(angulo_deg=41.5, potencia_max=100, perfil="encadenado")
@@ -316,7 +319,7 @@ robot.girar(angulo_deg=-70, potencia_max=100, perfil="encadenado")
 wait(80)
 robot.avanzar_cruzando_lineas(2,900, escape_inicial_cm=8,retraso_freno_ms=0,distancia_extra_cm=22)
 
-robot.girar(angulo_deg=56, potencia_max=100, perfil="encadenado")
+robot.girar(angulo_deg=55, potencia_max=100, perfil="encadenado")
 
 robot.seguir_linea(
     sensor_color=robot.seguidor,
@@ -453,7 +456,7 @@ robot.avanzar_hasta_color(color_objetivo=Color.BLUE, velocidad=500)
 robot.avanzar_recto(-9, 800)
 robot.girar(angulo_deg=-180, potencia_max=100, perfil="encadenado")
 
-robot.mover_torque(grados_torque=-164, velocidad_torque=300, esperar=False)
+robot.mover_torque(grados_torque=-164, velocidad_torque=400, esperar=False)
 robot.avanzar_recto(distancia_cm=-25, velocidad_max=800, perfil="seguro")
 
 # Salir de la seccion de azul y tomar la linea
@@ -467,7 +470,7 @@ robot.girar(angulo_deg=40, potencia_max=80)
 robot.seguir_linea(
     sensor_color=robot.seguidor,
     velocidad_max=100,
-    distancia_cm=127,
+    distancia_cm=126,
     lado="izquierda",
     tiempo_acomodo_ms=140,
     tiempo_aceleracion_ms=140,
@@ -487,7 +490,7 @@ robot.mover_torque(grados_torque=180, velocidad_torque=500, esperar=True)
 robot.seguir_linea(
     sensor_color=robot.seguidor,
     velocidad_max=70,
-    distancia_cm=13,
+    distancia_cm=14,
     lado="izquierda",
     tiempo_acomodo_ms=140,
     tiempo_aceleracion_ms=140,
@@ -504,9 +507,25 @@ robot.seguir_linea(
 )
 robot.avanzar_recto(4, 900)
 
-robot.avanzar_recto(-10.5, 900)
-wait(500)
-robot.girar(90, potencia_max=70)
+robot.avanzar_recto(-11, 900)
+
+wait(300)
+
+print("ANTES DEL GIRO:", robot.Hub.imu.heading())
+robot.girar(
+    angulo_deg=90,
+    potencia_max=55,
+    potencia_min=40,
+    kp_base=4.0,
+    kd_base=6.0,
+    tolerancia_fin=0.6,
+    perfil="seguro"
+)
+print("DESPUES DEL GIRO:", robot.Hub.imu.heading())
+
+wait(150)
+
+ejecutar_matriz_3(robot)
 
 # =================================================================================
 # INICIALIZACIÓN DE LA MATRIZ DETECTADA
@@ -515,10 +534,13 @@ robot.girar(90, potencia_max=70)
 # más arriba (guardado en matriz_detectada), se ejecuta el recorrido de la
 # matriz correspondiente. Por ahora solo está implementada la matriz 2.
 
+
+'''
 print("Matriz detectada:", matriz_detectada)
  
 if matriz_detectada == 2:
     print("Iniciando recorrido de la matriz 2...")
-    ejecutar_matriz_2(robot)
+    ejecutar_matriz_3(robot)
 else:
     print("No hay recorrido definido para la matriz detectada:", matriz_detectada)
+'''
