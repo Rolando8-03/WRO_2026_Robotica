ce6e8d3d19312254fa68c72ce4cbdcfff8786f58
elyGarcia <elgarcia@uamv.edu.ni>
2026-09-16 17:10:29 -0600

    Update matriz_3.py

--- matriz_3.py
+++ matriz_3.py
@@ -7,6 +7,7 @@ from control_drivebase import Base
 from pybricks.parameters import Color
 from pybricks.tools import wait
 import gc
+from matriz import dejar_bloques_matriz2
 
 
 def ejecutar_matriz_3(robot):
@@ -28,8 +29,8 @@ def ejecutar_matriz_3(robot):
 
     #Tomar los primeros dos cementos blancos y verdes ==========
     wait(400)
-    robot.avanzar_cruzando_lineas(cruces_objetivo=3, velocidad=700, distancia_extra_cm=12)
-    robot.mover_garra_principal(velocidad=900, grados=175, esperar=False)
+    robot.avanzar_cruzando_lineas(cruces_objetivo=3, velocidad=700, distancia_extra_cm=11.5)
+    robot.mover_garra_principal(velocidad=900, grados=180, esperar=False)
     wait(50)
     robot.girar(
         -91,
@@ -43,11 +44,11 @@ def ejecutar_matriz_3(robot):
     )
     wait(400)
     robot.avanzar_cruzando_lineas(cruces_objetivo=1, velocidad=-500)
-    robot.mover_garra_delantera(260, simultaneo=True)
+    robot.mover_garra_delantera(263, simultaneo=True)
 
-    robot.avanzar_recto(20, 800)
-    robot.mover_garra_principal(900, grados=40)
-    robot.avanzar_recto(-13.4)
+    robot.avanzar_recto(22, 800)
+    robot.mover_garra_principal(900, grados=75)
+    robot.avanzar_recto(-16.5)
     #giro para ir por los cementos amarillos ===========
     robot.girar(
         -89,
@@ -246,9 +247,9 @@ def ejecutar_matriz_3(robot):
     )
     robot.mover_garra_delantera(0, simultaneo=False)
 
-    robot.avanzar_recto(19, 600) #Avanzar para meter en los compartimentos los verdes
+    robot.avanzar_recto(20, 600) #Avanzar para meter en los compartimentos los verdes
     robot.mover_garra_delantera(260, simultaneo=False)
-    robot.avanzar_recto(-20)
+    robot.avanzar_recto(-20.5)
     robot.girar(
         90,
         potencia_max=80,
@@ -261,7 +262,29 @@ def ejecutar_matriz_3(robot):
     )
 
     #ir por los cementos blancos
-    robot.avanzar_recto(14.5)
+    robot.seguir_linea(
+        sensor_color=robot.seguidor,
+        distancia_cm=12,           
+        velocidad_max=100,         
+        lado="derecha",            
+        
+        tiempo_acomodo_ms=50,      
+        tiempo_aceleracion_ms=80,  
+        
+        #CEREBRO PREDICTIVO (PID):
+        kp=1.15,                   
+        kd=3.8,                    
+        k_freno=0.05,              
+        
+        correccion_max=100,
+        objetivo_reflexion=27,     
+        
+        captura_inicial=True,
+        tiempo_captura_ms=280,
+        potencia_captura=60,
+        kp_captura=2.5,
+        perfil_salida="encadenado"
+    )
     robot.girar(
         -90,
         potencia_max=80,
@@ -274,10 +297,13 @@ def ejecutar_matriz_3(robot):
     )
 
     robot.mover_garra_principal(velocidad=900, grados=130, esperar= True)
+    robot.girar_corto(13, potencia_max=50, potencia_min=34)
     robot.avanzar_recto(14)
     robot.mover_garra_principal(800, grados=0) #Aqui toma los blancos
+    robot.girar_corto(-10.5, potencia_max=50, potencia_min=34)
 
-    robot.avanzar_recto(-16.4)
+    robot.avanzar_recto(-23)
+    wait(100)
     robot.girar(
         -90,
         potencia_max=80,
@@ -288,22 +314,23 @@ def ejecutar_matriz_3(robot):
         tolerancia_fin=1.0,
         perfil="encadenado"
     )
+    robot.avanzar_recto(20)
     
-    robot.avanzar_cruzando_lineas(cruces_objetivo=1, velocidad=400, escape_inicial_cm=5, distancia_extra_cm=4)
 
-    wait(100)
+    wait(200)
     robot.girar(
         -180,
-        potencia_max=80,
-        potencia_min=45,
+        potencia_max=90,
+        potencia_min=50,
         kp_base=6.2,
         kd_base=3.5,
         tiempo_curva_s_ms=90,
         tolerancia_fin=1.0,
         perfil="encadenado"
     )
+    wait(100)
 
-    robot.avanzar_recto(distancia_cm=-17, velocidad_max=600, perfil="seguro")
+    robot.avanzar_recto(distancia_cm=-32, velocidad_max=600, perfil="seguro")
 
     robot.mover_torque(
         grados_torque=-170,
